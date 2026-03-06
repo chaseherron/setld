569bac27c1014ece66b3a49484d4a2955b97d8f4
chaseherron <chaserherron@gmail.com>
2026-03-05 22:26:02 -0500

    Chase Text to "take 3 question quiz" in button

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           href="/start"
           className="mt-10 bg-accent text-background font-bold text-lg sm:text-xl px-10 py-5 rounded-xl hover:brightness-110 transition-all inline-block"
         >
-          Take the 30-Second Quiz &rarr;
+          Take the 3 Question Quiz &rarr;
         </Link>
       </section>
 
